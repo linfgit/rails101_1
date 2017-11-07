be3809ef83a7f9a612bf68b60fc993f631880a85
linfgit <linfgg1@gmail.com>
2017-11-07 20:33:34 +0800

    when created a group, join it automatically

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106132547) do
+ActiveRecord::Schema.define(version: 20171107120944) do
+
+  create_table "group_relationships", force: :cascade do |t|
+    t.integer  "group_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
